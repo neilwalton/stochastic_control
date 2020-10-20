0ceb05a284575053a486ceadaecbe1aee0c00f0f
appliedprobability <neil.walton@appliedprobability.blog>
2020-10-20 10:26:22 +0100

    update to LQR

--- stochastic_control/optimal_control/LQR.py
+++ stochastic_control/optimal_control/LQR.py
@@ -4,3 +4,19 @@ import numpy as np
 Solve Riccati equation in various forms
 '''
 
+def riccati(L,A,B,C):
+    ''' riccati equation solver
+    
+    # Summary
+        One iteration of the riccati recursion for linear dynamic
+            x' = A x + B a
+        and cost
+            (x,a) C (x,a)
+        given value function at the next time is given by the matrix L
+    
+    # Arguments
+        L,C -- np.matrix (should be semi-definite)
+        A,B -- np.matrix
+    '''
+
+    dim_x = 
